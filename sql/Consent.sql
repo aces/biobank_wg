65c8b84889027ab7f6d3e8883f98a0f763baad9b
Krishna Chatpar <kchatpar@buffalo.edu>
2019-10-02 12:13:43 -0400

    added consent codes

--- sql/Consent.sql
+++ sql/Consent.sql
@@ -1,14 +1,26 @@
+/* Drop Consent table*/
 /* Adds consent form to database */
+INSERT INTO consent (Name, Label) VALUES ('IRB', 'Re-consented for latest version');
 
-INSERT INTO consent (Name, Label) VALUES ('Re-consent', 'I agree to be contacted by e-mail for follow-up information on C-BIG (otherwise information will be made available on the C-BIG website)');
-INSERT INTO consent (Name, Label) VALUES ('Contact', 'I agree to be contacted by e-mail for follow-up information on C-BIG (otherwise information will be made available on the C-BIG website)');
-INSERT INTO consent (Name, Label) VALUES ('Dossier', 'I agree for the clinical research co-coordinators to access my dossier de sante du Quebec or institutional clinical repository to obtain clinical, laboratory, and radiological data relevant for C-BIG periodically and/or as needed');
-INSERT INTO consent (Name, Label) VALUES ('CellLines', 'I agree that my sample be used to generate cell lines');
-INSERT INTO consent (Name, Label) VALUES ('Questionaires', 'I agree to be re-contacted to complete additional questionnaires about my health and quality of life');
-INSERT INTO consent (Name, Label) VALUES ('Blood', 'I agree to be re-contacted for additional sampling (blood, skin and muscle biopsy)');
-INSERT INTO consent (Name, Label) VALUES ('PI', 'I agree to be re-contacted for future specific research projects if deemed warranted by the projects principal investigator / clinician');
+INSERT INTO consent (Name, Label) VALUES ('INF', 'I agree to be contacted by e-mail for follow-up information on C-BIG (otherwise information will be made available on the C-BIG website)');
+
+INSERT INTO consent (Name, Label) VALUES ('HR', 'I agree for the clinical research co-coordinators to access my dossier de sante du Quebec or institutional clinical repository to obtain clinical, laboratory, and radiological data relevant for C-BIG periodically and/or as needed');
+
+INSERT INTO consent (Name, Label) VALUES ('GEN', 'I agree that my samples be used to extract DNA, RNA and micro-RNA');
+
+
+INSERT INTO consent (Name, Label) VALUES ('CL', 'I agree that my sample be used to generate cell lines');
+
+INSERT INTO consent (Name, Label) VALUES ('CQ', 'I agree to be re-contacted to complete additional questionnaires about my health and quality of life');
+
+INSERT INTO consent (Name, Label) VALUES ('CS', 'I agree to be re-contacted for additional sampling (blood, skin and muscle biopsy)');
+
+INSERT INTO consent (Name, Label) VALUES ('CR', 'I agree to be re-contacted for future specific research projects if deemed warranted by the projects principal investigator / clinician');
+
 INSERT INTO consent (Name, Label) VALUES ('LP', 'I consent to participate to undergo an LP for the purpose of C-BIG in accordance with the conditions stated.');
-INSERT INTO consent (Name, Label) VALUES ('Biopsy', 'I consent to participate to undergo a muscle biopsy for the purpose of C-BIG in accordance with the conditions stated.');
-INSERT INTO consent (Name, Label) VALUES ('Skin', 'I consent to participate to undergo a skin biopsy for the purpose of C-BIG in accordance with the conditions stated.');
+
+INSERT INTO consent (Name, Label) VALUES ('LPMB', 'I consent to participate to undergo a muscle biopsy for the purpose of C-BIG in accordance with the conditions stated.');
+
+INSERT INTO consent (Name, Label) VALUES ('LPSB', 'I consent to participate to undergo a skin biopsy for the purpose of C-BIG in accordance with the conditions stated.');
 
 
